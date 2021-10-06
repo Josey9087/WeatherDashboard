7771d6d014fbf4cf63d1c1caec7ad2221714dee3
Josey9087 <88466226+Josey9087@users.noreply.github.com>
2021-10-06 01:01:27 -0600

    Trying to figure out API

--- script.js
+++ script.js
@@ -1,4 +1,35 @@
 var WeatherDashboardAPIKey = "27127e631022b8f861292951b0145e8c";
 var city="Denver"
-call= api.openweathermap.org/data/2.5/weather?q={city}&appid:{WeatherDashboardAPIKey}
-fetch(call)
+call= "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + WeatherDashboardAPIKey;
+
+lat="39.7392"
+lon="-104.9847"
+function getApi() {
+    call= "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + WeatherDashboardAPIKey;
+  fetch(call)
+    .then(function (response) {
+        console.log(response)
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      lat=data.coord.lat
+      lon=data.coord.lon
+    })
+    }
+
+  
+function get5days(){
+    call1="https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=" +WeatherDashboardAPIKey;
+    fetch(call1)
+    .then(function(response){
+        console.log(response)
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+    })}
+
+
+getApi()
+get5days()
